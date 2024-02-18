5078360fe0c43d559653035e84d63530f4b3022e
Talha Hori <talha.solutionq786@gmail.com>
2024-02-18 17:35:08 +0500

    FAQS Page

---
+++ assets/js/common/faqs.js
@@ -0,0 +1,17 @@
+$('.demo').popr({
+  'speed': 200,
+  'mode': 'bottom'
+});
+
+
+$(document).ready(function(){
+ $(".accordion").on("click", ".heading", function() {
+
+ $(this).toggleClass("active").next().slideToggle();
+
+ $(".contents").not($(this).next()).slideUp(300);
+              
+ $(this).siblings().removeClass("active");
+ });
+});
+           
