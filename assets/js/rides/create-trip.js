2d2962f8e0fb2106e5b574a94eb36e73224e1fc3
Talha Hori <talha.solutionq786@gmail.com>
2024-02-26 03:36:36 +0500

    Changes

---
+++ assets/js/rides/create-trip.js
@@ -0,0 +1,14 @@
+$('.demo').popr({
+  'speed': 200,
+  'mode': 'bottom'
+});
+
+// $(document).ready(function() {
+//   const urlParams = new URLSearchParams(window.location.search);
+//   const _id = parseInt(urlParams.get('id')) || 0; 
+//   console.log({_id});
+//   if (_id < 1) {
+//     $('.isEdit').text('Add Daily Commute');
+//     $('.for_new').css('display', 'block');
+//   }
+// })
